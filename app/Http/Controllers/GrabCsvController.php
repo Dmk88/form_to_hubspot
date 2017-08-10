4c8f2b219df295285bed2bdb332808d480e85262
Dmitry Mikhalchenko <dm.mail@tut.by>
2017-08-10 13:46:33 +0300

    Mod GrabCsvController

--- app/Http/Controllers/GrabCsvController.php
+++ app/Http/Controllers/GrabCsvController.php
@@ -74,36 +74,17 @@ class GrabCsvController extends Controller
         if(!$id){
             return;
         }
-        // $response = HubSpot::forms()->getById('454a0ba9-449a-4e10-aeda-e8116b02e4b2');
-        // dd($response);exit;
         define('APPLICATION_NAME', 'Google Sheets API PHP Quickstart');
         define('CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-php-quickstart.json');
         define('CLIENT_SECRET_PATH', app_path() . '/client_secret.json');
-        // If modifying these scopes, delete your previously saved credentials
-        // at ~/.credentials/sheets.googleapis.com-php-quickstart.json
         define('SCOPES', implode(' ', array(
             Google_Service_Sheets::SPREADSHEETS_READONLY,
         )));
         $client  = $this->getClient();
         $service = new Google_Service_Sheets($client);
-        
-        // Prints the names and majors of students in a sample spreadsheet:
-        // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-        $spreadsheetId = '104x2cj6Sf0alMZj69hZTwwVlbmmeXYz3vyTT022uxAw';
-        $range         = 'A2:M23';
-        // $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-        // $values = $response->getValues();
-        
-        // $response = $service->spreadsheets->get($spreadsheetId);
-        // $response_sheets = $response->getSheets();
-        // foreach ($response_sheets as $response_sheet){
-        //     $response_data[] = $response_sheet->getData();
-        // }
+
         $response = $service->spreadsheets_values->get($id, 'A2:Z');
-        // dd($id, $response->values);exit;
-        // $response =
         $portal_id               = '2950617';
-        // $form_guid               = '454a0ba9-449a-4e10-aeda-e8116b02e4b2';
         $google_doc_to_form_guid = [
             '104x2cj6Sf0alMZj69hZTwwVlbmmeXYz3vyTT022uxAw' => '454a0ba9-449a-4e10-aeda-e8116b02e4b2', // Jenkins for PCF
             '14anTELce-L5WIOfh28lsy5DnnXhoM4LPJGtLNHFpVs8' => 'ae50b393-62e3-43b4-b78d-fa99f10fb77b', // AWS S3 for PCF
@@ -127,11 +108,6 @@ class GrabCsvController extends Controller
                 'release'      => $form_data[10],
             ];
             $hubspot_req = HubSpot::forms()->submit($portal_id, $google_doc_to_form_guid[$id], $form);
-            // echo"<pre>";
-            // var_dump($hubspot_req->response->statusCode);
-            // dd($hubspot_req, gettype($hubspot_req), $hubspot_req);
-            // dd($hubspot_req->response);
-            // exit;
             
             $form_data = FormData::create([
                 'email'         => $form_data[0],
@@ -147,42 +123,5 @@ class GrabCsvController extends Controller
         echo "<pre>";
         var_dump($response->values);
         exit;
-        
-        
-        $client = new Google_Client();
-        $client->setApplicationName(APPLICATION_NAME);
-        $client->setScopes(SCOPES);
-        $client->setAuthConfig(CLIENT_SECRET_PATH);
-        $service = new Google_Service_Sheets($client);
-        
-        $spreadsheetId = '157JP5drsBTda98qfRn8pkgjUrSa93dUlWCYw9TTLOcU';
-        $range         = 'A2:M23';
-        $response      = $service->spreadsheets_values->get($spreadsheetId, $range);
-        $values        = $response->getValues();
-        
-        dd($values);
-        exit;
-        // Get the API client and construct the service object.
-        $client = $this->getClient();
-        dd($client);
-        exit;
-        $service = new Google_Service_Sheets($client);
-        
-        
-        // $posts = FormData::get();
-        // dd(env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION'));exit;
-        $mail__poet       = '157JP5drsBTda98qfRn8pkgjUrSa93dUlWCYw9TTLOcU';
-        $mail__poet_sheet = 'Sheet1';
-        $csv              = '104x2cj6Sf0alMZj69hZTwwVlbmmeXYz3vyTT022uxAw';
-        $csv_sheet        = 'altoros-jenkins-for-pcf-downloads.csv';
-        
-        Sheets::setService(Google::make('sheets'));
-        Sheets::spreadsheet($mail__poet);
-        $values = Sheets::sheet($mail__poet_sheet);
-        $d      = $values->all();
-        dd($values, $d);
-        // var_dump($values);
-        // exit;
-        // return View::make('index')->with('posts', $posts);
     }
 }
