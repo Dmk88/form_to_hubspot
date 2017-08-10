328ba74328298e69e48fa6224dec4066876badf2
Dmitry Mikhalchenko <dm.mail@tut.by>
2017-08-10 17:07:30 +0300

    Add deny organisations: 'altoros', 'avarteq', 'ecs'

--- app/Http/Controllers/GrabCsvController.php
+++ app/Http/Controllers/GrabCsvController.php
@@ -71,7 +71,7 @@ class GrabCsvController extends Controller
     
     public function index($id = '')
     {
-        if(!$id){
+        if (!$id) {
             return;
         }
         define('APPLICATION_NAME', 'Google Sheets API PHP Quickstart');
@@ -82,20 +82,27 @@ class GrabCsvController extends Controller
         )));
         $client  = $this->getClient();
         $service = new Google_Service_Sheets($client);
-
-        $response = $service->spreadsheets_values->get($id, 'A2:Z');
+        
+        $response                = $service->spreadsheets_values->get($id, 'A2:Z');
         $portal_id               = '2950617';
         $google_doc_to_form_guid = [
-            '104x2cj6Sf0alMZj69hZTwwVlbmmeXYz3vyTT022uxAw' => '454a0ba9-449a-4e10-aeda-e8116b02e4b2', // Jenkins for PCF
-            '14anTELce-L5WIOfh28lsy5DnnXhoM4LPJGtLNHFpVs8' => 'ae50b393-62e3-43b4-b78d-fa99f10fb77b', // AWS S3 for PCF
-            '1M2AWLrjF_c2CvMMqn5z1JkrhByQly0BGywfePqa9STw' => '009d03a1-d5c6-45e1-9e06-1e750f2ffc5d', // Cassandra for PCF
-            '1STHWs6T8Ba_yO02NYsrPHsXiG2gTtCYe1ORoW3q3gk4' => 'f3d3658f-ea0a-4799-810d-c905d953752d', // Log Search for PCF
-            '1HrQFE2es9pOZLbL6zZZ6TkwBh02NtCCzarCdWPcAddo' => '8bf64175-a8b9-4a32-a0ef-a06b2ead168a', // Elasticsearch for PCF
-            '1zFwbOtYrY-ZHonNJh6egsR_AHIEUKvdmxNUW545QlMg' => '9aefc59b-9041-4304-8a65-05aa2c13a55d', // Heartbeat for PCF
+            '104x2cj6Sf0alMZj69hZTwwVlbmmeXYz3vyTT022uxAw' => '454a0ba9-449a-4e10-aeda-e8116b02e4b2',
+            // Jenkins for PCF
+            '14anTELce-L5WIOfh28lsy5DnnXhoM4LPJGtLNHFpVs8' => 'ae50b393-62e3-43b4-b78d-fa99f10fb77b',
+            // AWS S3 for PCF
+            '1M2AWLrjF_c2CvMMqn5z1JkrhByQly0BGywfePqa9STw' => '009d03a1-d5c6-45e1-9e06-1e750f2ffc5d',
+            // Cassandra for PCF
+            '1STHWs6T8Ba_yO02NYsrPHsXiG2gTtCYe1ORoW3q3gk4' => 'f3d3658f-ea0a-4799-810d-c905d953752d',
+            // Log Search for PCF
+            '1HrQFE2es9pOZLbL6zZZ6TkwBh02NtCCzarCdWPcAddo' => '8bf64175-a8b9-4a32-a0ef-a06b2ead168a',
+            // Elasticsearch for PCF
+            '1zFwbOtYrY-ZHonNJh6egsR_AHIEUKvdmxNUW545QlMg' => '9aefc59b-9041-4304-8a65-05aa2c13a55d',
+            // Heartbeat for PCF
         ];
+        $deny_organizations      = ['altoros', 'avarteq', 'ecs'];
         
         foreach ($response->values as $form_data) {
-            if(strtolower($form_data[9]) !== 'software'){
+            if ((strtolower($form_data[9]) !== 'software') || (in_array(strtolower($form_data[3]), $deny_organizations))) {
                 continue;
             }
             $form        = [
